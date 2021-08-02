404a9d348b913d05719b5e93ddcef85d24276990
Tyler <tythedeveloper10@gmail.com>
2021-08-02 05:40:00 -0500

    update background image

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import './App.css';
-import Header from './Header/Header';
-import React, {useState} from 'react';
+import "./App.css";
+import Header from "./Header/Header";
+import React, { useState } from "react";
 
 const App = () => {
-  const titleText = "Burger Restaurant"
+  const titleText = "Big Kahuna Burger";
   const [cartCount, setCartCount] = useState(0);
 
-  return (
-    <Header titleText={titleText} cartCount={cartCount}/>
-  );
-}
+  return <Header titleText={titleText} cartCount={cartCount} />;
+};
 
 export default App;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
-import Badge from '@material-ui/core/Badge'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Icon from "@material-ui/core/Icon";
+import IconButton from "@material-ui/core/IconButton";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import Badge from "@material-ui/core/Badge";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,28 +14,32 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1,
-    marginLeft: theme.spacing(4),
+    marginLeft: theme.spacing(2),
+  },
+  backgroundColor: {
+    backgroundColor: "#A52A2A",
   },
 }));
 
 const Header = (props) => {
-    const classes = useStyles();
-    return (
+  const classes = useStyles();
+  return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.backgroundColor}>
         <Toolbar>
+          <Icon>lunch_dining</Icon>
           <Typography variant="h6" className={classes.title}>
             {props.titleText}
           </Typography>
           <IconButton color="inherit">
-              <Badge badgeContent={props.cartCount} color="secondary">
-                <ShoppingCartIcon />
-              </Badge>
+            <Badge badgeContent={props.cartCount} color="secondary">
+              <ShoppingCartIcon />
+            </Badge>
           </IconButton>
         </Toolbar>
       </AppBar>
     </div>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
