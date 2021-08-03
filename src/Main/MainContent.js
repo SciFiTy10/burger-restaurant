32c25024a1cfd53d4d2e885c1a41299c42eb0827
Tyler <tythedeveloper10@gmail.com>
2021-08-03 06:41:20 -0500

    finish updating background image and complete welcome card

--- src/Main/MainContent.js
+++ src/Main/MainContent.js
@@ -1,11 +1,29 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
+import { Box, Grid, Hidden } from "@material-ui/core";
 import Welcome from "./Welcome";
+import BackgroundImage from "../Assets/burger.jpg";
 
 const MainContent = () => {
+  const styles = {
+    root: {
+      backgroundImage: `url(${BackgroundImage})`,
+      backgroundRepeat: "space repeat",
+      backgroundSize: "90px 90px",
+    },
+  };
   return (
-    <Grid container>
-      <Welcome />
+    <Grid container style={styles.root}>
+      <Hidden xsDown>
+        <Grid item md={3}></Grid>
+      </Hidden>
+      <Grid item xs={12} md={6}>
+        <Box p={10}>
+          <Welcome />
+        </Box>
+      </Grid>
+      <Hidden xsDown>
+        <Grid item md={3}></Grid>
+      </Hidden>
     </Grid>
   );
 };
--- src/Main/Welcome.js
+++ src/Main/Welcome.js
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
-import { Card, CardHeader } from "@material-ui/core";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Grid,
+  Typography,
+} from "@material-ui/core";
 
 const Welcome = () => {
   const [title, setTitle] = useState(
     "Welcome to the best burger restaurant around!"
   );
+
+  const cardTitle = {
+    textAlign: "center",
+  };
   return (
     <Card>
-      <CardHeader title={title}></CardHeader>
+      <CardHeader
+        title={title}
+        titleTypographyProps={{ align: "center" }}
+      ></CardHeader>
+      <CardContent>
+        <Grid container justifyContent="center">
+          <Grid item>
+            <Typography variant="h6" component="p" align="center">
+              Choose one of our delicious, gourmet burgers prepared by our top
+              tier chef.
+            </Typography>
+            <Typography variant="h6" component="p" align="center">
+              Satisfaction is guaranteed. If you don't like our burgers, not
+              only will we give you your money back, but we will personally
+              throw you from our restaurant!
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
 };
