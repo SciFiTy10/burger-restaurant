ada9a7d21a133cee8b7d4c3a99cc44569c5f60ea
Tyler <tythedeveloper10@gmail.com>
2021-09-01 16:01:37 -0500

    finish add item scenarios

--- src/Components/Custom/Item/Amount/Test/AddItem/addItem.test.js
+++ src/Components/Custom/Item/Amount/Test/AddItem/addItem.test.js
@@ -6,8 +6,16 @@ import { cartReducer } from "../../../../../../Reducers/cartReducer/cartReducer"
 import userEvent from "@testing-library/user-event";
 
 describe("adding an item", () => {
-  //create initial cart state
-  let cart = [];
+  //create an empty cart
+  let cart = [
+    {
+      id: 1,
+      title: "Royale With Cheese",
+      price: 9.99,
+      amount: 1,
+    },
+  ];
+
   //handler function for adding an item to the cart
   const cartAddHandler = (item) => {
     //ensure the item is non-empty
@@ -16,15 +24,26 @@ describe("adding an item", () => {
       cart = cartReducer(cart, { type: "ADD_ITEM", payload: item });
     }
   };
-  //create a test item to be added
-  const testAddItem = {
-    id: 1,
-    title: "Royale With Cheese",
-    price: 9.99,
-    amount: 1,
-  };
+
+  afterEach(() => {
+    //reset the cart value
+    cart = [
+      {
+        id: 1,
+        title: "Royale With Cheese",
+        price: 9.99,
+        amount: 1,
+      },
+    ];
+  });
 
   it("should add a new item to the cart", () => {
+    const itemToBeAdded = {
+      id: 2,
+      title: "What Burger",
+      price: 10.99,
+      amount: 1,
+    };
     render(
       <AppContext.Provider
         value={{
@@ -32,10 +51,10 @@ describe("adding an item", () => {
         }}
       >
         <ItemAmount
-          id={testAddItem.id}
-          title={testAddItem.title}
-          price={testAddItem.price}
-          amount={testAddItem.amount}
+          id={itemToBeAdded.id}
+          title={itemToBeAdded.title}
+          price={itemToBeAdded.price}
+          amount={itemToBeAdded.amount}
           cartAddHandler={cartAddHandler}
         />
       </AppContext.Provider>
@@ -48,6 +67,47 @@ describe("adding an item", () => {
         title: "Royale With Cheese",
         price: 9.99,
         amount: 1,
+      },
+      {
+        id: 2,
+        title: "What Burger",
+        price: 10.99,
+        amount: 1,
+      },
+    ]);
+  });
+
+  it("should increment the amount if already in the cart", () => {
+    //create a test item to be added
+    const itemToBeAdded = {
+      id: 1,
+      title: "Royale With Cheese",
+      price: 9.99,
+      amount: 1,
+    };
+    render(
+      <AppContext.Provider
+        value={{
+          cart,
+        }}
+      >
+        <ItemAmount
+          id={itemToBeAdded.id}
+          title={itemToBeAdded.title}
+          price={itemToBeAdded.price}
+          amount={itemToBeAdded.amount}
+          cartAddHandler={cartAddHandler}
+        />
+      </AppContext.Provider>
+    );
+
+    userEvent.click(screen.getByLabelText("add one of item"));
+    expect(cart).toEqual([
+      {
+        id: 1,
+        title: "Royale With Cheese",
+        price: 9.99,
+        amount: 2,
       },
     ]);
   });
