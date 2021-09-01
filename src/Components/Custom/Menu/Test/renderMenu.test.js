ae60fb401dc23cb4c6f91353a21981f4aabcd2be
Tyler <tythedeveloper10@gmail.com>
2021-09-01 11:15:06 -0500

    update menu render test

--- src/Components/Custom/Menu/Test/renderMenu.test.js
+++ src/Components/Custom/Menu/Test/renderMenu.test.js
@@ -4,50 +4,51 @@ import { AppContext } from "../../../../Context/app-context";
 import Menu from "../Menu";
 
 describe("menu", () => {
+  //create a menu array
+  const menu = [];
+  //create an empty cart
+  const cart = [];
+  beforeAll(() => {
+    //create a menu
+    menu.push(
+      {
+        id: 1,
+        title: "Royale With Cheese",
+        description: "You know what they call a quarter pounder in France?",
+        price: 9.99,
+      },
+      {
+        id: 2,
+        title: "What Burger",
+        description:
+          "What ain't no burger I ever heard of, they speak English in what?",
+        price: 10.99,
+      },
+      {
+        id: 3,
+        title: "Marvin Special",
+        description: "The only burger in town without a top bun",
+        price: 8.99,
+      }
+    );
+  });
+  afterAll(() => {
+    //destroy the menu
+    menu.splice(0, menu.length);
+  });
   test("renders Menu component", () => {
-    //create a menu array
-    const menu = [];
-    beforeAll(() => {
-      //create a menu
-      menu.push(
-        {
-          id: 1,
-          title: "Royale With Cheese",
-          description: "You know what they call a quarter pounder in France?",
-          price: 9.99,
-        },
-        {
-          id: 2,
-          title: "What Burger",
-          description:
-            "What ain't no burger I ever heard of, they speak English in what?",
-          price: 10.99,
-        },
-        {
-          id: 3,
-          title: "Marvin Special",
-          description: "The only burger in town without a top bun",
-          price: 8.99,
-        }
-      );
-    });
-    afterAll(() => {
-      //destroy the menu
-      menu.splice(0, menu.length);
-    });
-
-    const wrapper = ({ children }) => (
+    render(
       <AppContext.Provider
         value={{
           menu,
+          cart,
         }}
       >
-        {children}
+        <Menu />
       </AppContext.Provider>
     );
 
-    render(<Menu />, { wrapper });
-
+    screen.debug();
     expect(screen.getByText("Royale With Cheese")).toBeInTheDocument();
   });
 });
