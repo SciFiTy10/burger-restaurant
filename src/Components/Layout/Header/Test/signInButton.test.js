b98d1beb03cad5b983c6e3f1eff7547312a83675
Tyler <tythedeveloper10@gmail.com>
2021-10-01 21:13:06 -0500

    update test

--- src/Components/Layout/Header/Test/signInButton.test.js
+++ src/Components/Layout/Header/Test/signInButton.test.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Header from "../Header";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HomePage from "../../../../Pages/HomePage";
+import SignInPage from "../../../../Pages/SignInPage";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
+import App from "../../../../App";
 
 describe("sign in button", () => {
   it("takes the user to the sign in page", () => {
-    const { getByLabelText } = render(
-        <Header />
-    );
+    const { getByLabelText } = render(<App />);
 
     //click the cart button
     userEvent.click(getByLabelText("sign in button"));
@@ -16,26 +17,26 @@ describe("sign in button", () => {
     expect(screen.getByText("Sign In")).toBeInTheDocument();
   });
 
-//   it("renders a cart with an item", () => {
-//     const { getByLabelText, getByTestId } = render(
-//       <AppContextProvider>
-//         <Header />
-//         <Menu />
-//         <DialogContainer />
-//       </AppContextProvider>
-//     );
+  //   it("renders a cart with an item", () => {
+  //     const { getByLabelText, getByTestId } = render(
+  //       <AppContextProvider>
+  //         <Header />
+  //         <Menu />
+  //         <DialogContainer />
+  //       </AppContextProvider>
+  //     );
 
-//     //add an item from the menu
-//     userEvent.click(getByTestId("add one Royale With Cheese menu 1"));
+  //     //add an item from the menu
+  //     userEvent.click(getByTestId("add one Royale With Cheese menu 1"));
 
-//     //click the cart button
-//     userEvent.click(getByLabelText("shopping cart button"));
+  //     //click the cart button
+  //     userEvent.click(getByLabelText("shopping cart button"));
 
-//     //there should be a count of 1 for the item in the cart
-//     expect(
-//       screen
-//         .getByTestId("amount of Royale With Cheese cart 1")
-//         .querySelector("input")
-//     ).toHaveValue("1");
-//   });
-// });
+  //     //there should be a count of 1 for the item in the cart
+  //     expect(
+  //       screen
+  //         .getByTestId("amount of Royale With Cheese cart 1")
+  //         .querySelector("input")
+  //     ).toHaveValue("1");
+  //   });
+});
